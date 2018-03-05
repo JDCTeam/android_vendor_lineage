124a9bb7307bd3ec420ffc6a4333fe5367c5bb5f
dkati <dkatikaridis@gmail.com>
2018-03-05 23:54:56 +0200

    fo real ?
    Change-Id: I660d3613d3f8b240d9a763af0f59108fda822155

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -10,7 +10,7 @@ lineage_soong:
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "BTVendorPath": "$(call project-path-for,bt-vendor)",'; \
 	echo '    "RILPath": "$(call project-path-for,ril)",'; \
-	echo '    "WLANPath": "$(call project-path-for,wlan)"'; \
+	echo '    "WLANPath": "$(call project-path-for,wlan)",'; \
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)'; \
 	echo '},'; \
