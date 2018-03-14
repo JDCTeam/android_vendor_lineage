428015b5587ec3625027885710b562d804c55204
dkati <dkatikaridis@gmail.com>
2018-03-14 17:27:19 +0200

    soong:Tide up the variables
    Change-Id: I9469c8b8effad22fdc4bcf99aafa0533b88ec8e1

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -9,35 +9,6 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
-	Uses_non_treble_camera struct {
-		Cflags []string
-	}
-	BoardUsesQTIHardware struct {
-		Cflags []string
-		Cppflags []string
-	}
-	BoardUsesQCOMHardware struct {
-		Cflags []string
-		Cppflags []string
-	}
-	TargetUsesQCOMBsp struct {
-		Cflags []string
-		Cppflags []string
-	}
-	TargetUsesQCOMLegacyBsp struct {
-		Cflags []string
-		Cppflags []string
-	}
-	BoardUsesLegacyAlsa struct {
-		Cflags []string
-		Cppflags []string
-	}
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-	Uses_media_extensions struct {
-		Cflags []string
-	}
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
@@ -45,14 +16,6 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
-        Uses_non_treble_camera  *bool `json:",omitempty"`
-	BoardUsesQTIHardware  *bool `json:",omitempty"`
-	BoardUsesQCOMHardware  *bool `json:",omitempty"`
-	TargetUsesQCOMBsp  *bool `json:",omitempty"`
-	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
-	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
-        Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
