80e4eabe5956805e867636aa188666b0afd6af68
dkati <dkatikaridis@gmail.com>
2018-04-06 01:27:41 +0300

    soong:Add TARGET_USES_NON_TREBLE_CAMERA
    Change-Id: I46ed1f7b94243b017fe175e8e72de52f06b202ce

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -22,6 +22,9 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
+	Uses_non_treble_camera struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -30,6 +33,7 @@ type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_media_extensions  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -11,6 +11,7 @@ lineage_soong:
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
 	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
 	echo '},'; \
 echo '') > $(SOONG_VARIABLES_TMP)
