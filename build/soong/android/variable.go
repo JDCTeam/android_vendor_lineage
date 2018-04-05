7c94e5360a5466bbbe4c279d6ada0883c9b0a7fa
dkati <dkatikaridis@gmail.com>
2018-04-06 01:25:34 +0300

    soong: add TARGET_HAS_LEGACY_CAMERA_HAL1 to soong variables
    Change-Id: I808a831c03fe1877a2b60e40c8b45c88effed82a

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -19,6 +19,9 @@ type Product_variables struct {
 	Uses_media_extensions struct {
 		Cflags []string
 	}
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -26,6 +29,7 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_media_extensions  *bool `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
